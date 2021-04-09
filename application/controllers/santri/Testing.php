1aab37f89f2fda06ff607126cd7b46ae3d381f03
Isep Lutpii Nur <iseplutpinur7@gmail.com>
2021-04-10 06:59:33 +0700

    Tampilkan data tahun ajaran

---
+++ application/controllers/santri/Testing.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Testing extends Render_Controller
+{
+    public function index()
+    {
+        echo "hello mantan";
+    }
+}
